e136a428fa98dd0e1660376ff25c4140b8bc7ce8
dsinjok <dsinjok@163.com>
2018-03-27 16:15:17 +0800

    master
    master

--- WavHdr.h
+++ WavHdr.h
@@ -4,8 +4,8 @@
 #include<stdio.h>
 #include<string.h>
 
-typedef unsigned long       SR_DWORD;
-typedef unsigned short      SR_WORD;
+typedef unsigned long       SR_DWORD;		//占4字节 
+typedef unsigned short      SR_WORD;		//占2字节 
 
 struct wavhdr{
     char            riff[4];                        // = "RIFF"
@@ -15,14 +15,14 @@ struct wavhdr{
     SR_DWORD        dwFmtSize;                      // = 下一个结构体的大小 : 16
 
     SR_WORD         format_tag;              		// = PCM : 1
-    SR_WORD         channels;                		// = 通道数 : 1
-    SR_DWORD        samples_per_sec;         		// = 采样率 : 8000 | 6000 | 11025 | 16000
-    SR_DWORD        avg_bytes_per_sec;       		// = 每秒字节数 : dwSamplesPerSec * wBitsPerSample / 8
+    SR_WORD         channels;                		// = 通道数 : 1单声道,2双声道 
+    SR_DWORD        samples_per_sec;         		// = 采样率 
+    SR_DWORD        avg_bytes_per_sec;       		// = 每秒字节数 
     SR_WORD         block_align;             		// = 每采样点字节数 : wBitsPerSample / 8
     SR_WORD         bits_per_sample;         		// = 量化比特数: 8 | 16
 
     char            data[4];                 		// = "data";
-    SR_DWORD        data_size;               		// = 纯数据长度 : FileSize - 44 
+    SR_DWORD        data_size;               		// = 不含文件头 文件长度 
 } ;
 
 int wav_hdr_reader(FILE *fp, struct wavhdr	*hdr)
